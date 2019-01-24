00beb5bfb9e9e56011f249839143919bcff490e1
fudh <abc10086>
2019-01-24 11:24:25 +0800

    发送普通文本

--- src/main/java/com/fdh/simulator/listenner/PacketLisenner.java
+++ src/main/java/com/fdh/simulator/listenner/PacketLisenner.java
@@ -1,52 +1,52 @@
-package com.fdh.simulator.listenner;
-
-import com.fdh.simulator.ExpirieListenner;
-import com.fdh.simulator.NettyChannelManager;
-import com.fdh.simulator.PacketAnalyze;
-import com.fdh.simulator.Simulator;
-import com.fdh.simulator.utils.ReportUtils;
-import com.fdh.simulator.utils.SpringContextUtils;
-import net.jodah.expiringmap.ExpirationListener;
-import net.jodah.expiringmap.ExpiringMap;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-
-/**
- * @author fudh
- * @ClassNmme PacketLisenner
- * @date 2019/1/22 11:48
- * @Description: 发送的数据包过期策略
- */
-public class PacketLisenner implements ExpirationListener<Integer, Long> {
-
-    private static final Logger logger = LoggerFactory.getLogger(ExpirieListenner.class);
-
-    @Override
-    public void expired(Integer packetSerailNum, Long timestamp) {
-        ExpiringMap<Integer, Long> sendPacketMap = PacketAnalyze.sendPacketMap;
-//        int expiredCount = PacketAnalyze.expireSendCount.incrementAndGet();
-//        int packetCount = PacketAnalyze.atomicLong.get();
-//        logger.error("sendPacketMap->"+sendPacketMap.size());
-//        logger.info("expiredCount->"+expiredCount+"------packetCount"+packetCount);
-        if (sendPacketMap.size() == 0) {
-            logger.error("***********************************************************测试完成***********************************************************");
-            Simulator.timer.cancel();
-            //断开所有的连接不在接收报文
-            NettyChannelManager.removeAll();
-            ReportUtils.report();
-            ConcurrentHashMap<Integer, Integer> receiveMap = PacketAnalyze.receiveMap;
-            Set<Map.Entry<Integer, Integer>> entries = receiveMap.entrySet();
-            for (Map.Entry<Integer, Integer> entry : entries) {
-                Integer key = entry.getKey();
-                Integer value = entry.getValue();
-                logger.error("数据包："+key+"过期了，累计耗时："+value);
-            }
-
-        }
-
-    }
-}
+//package com.fdh.simulator.listenner;
+//
+//import com.fdh.simulator.ExpirieListenner;
+//import com.fdh.simulator.NettyChannelManager;
+//import com.fdh.simulator.PacketAnalyze;
+//import com.fdh.simulator.Simulator;
+//import com.fdh.simulator.utils.ReportUtils;
+//import com.fdh.simulator.utils.SpringContextUtils;
+//import net.jodah.expiringmap.ExpirationListener;
+//import net.jodah.expiringmap.ExpiringMap;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import java.util.Map;
+//import java.util.Set;
+//import java.util.concurrent.ConcurrentHashMap;
+//
+///**
+// * @author fudh
+// * @ClassNmme PacketLisenner
+// * @date 2019/1/22 11:48
+// * @Description: 发送的数据包过期策略
+// */
+//public class PacketLisenner implements ExpirationListener<Integer, Long> {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(ExpirieListenner.class);
+//
+//    @Override
+//    public void expired(Integer packetSerailNum, Long timestamp) {
+//        ExpiringMap<Integer, Long> sendPacketMap = PacketAnalyze.sendPacketMap;
+////        int expiredCount = PacketAnalyze.expireSendCount.incrementAndGet();
+////        int packetCount = PacketAnalyze.atomicLong.get();
+////        logger.error("sendPacketMap->"+sendPacketMap.size());
+////        logger.info("expiredCount->"+expiredCount+"------packetCount"+packetCount);
+//        if (sendPacketMap.size() == 0) {
+//            logger.error("***********************************************************测试完成***********************************************************");
+//            Simulator.timer.cancel();
+//            //断开所有的连接不在接收报文
+//            NettyChannelManager.removeAll();
+//            ReportUtils.report();
+//            ConcurrentHashMap<Integer, Integer> receiveMap = PacketAnalyze.receiveMap;
+//            Set<Map.Entry<Integer, Integer>> entries = receiveMap.entrySet();
+//            for (Map.Entry<Integer, Integer> entry : entries) {
+//                Integer key = entry.getKey();
+//                Integer value = entry.getValue();
+//                logger.error("数据包："+key+"过期了，累计耗时："+value);
+//            }
+//
+//        }
+//
+//    }
+//}
